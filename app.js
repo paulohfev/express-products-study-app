55ba9c8b0fead315b3fc0ef50133d2a31ea2bddc
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2023-04-19 17:56:22 -0300

    Create 404 and add-product pug files

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ app.set('views', 'views');
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404');
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -7,17 +7,15 @@ const rootDir = require('../utils/path');
 const router = express.Router();
 
 const products = [];
-console.log(products)
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', { pageTitle: 'Add Product' });
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
   products.push({ title: req.body.title });
-  console.log(products)
   res.redirect('/');
 });
 

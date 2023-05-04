4982bb8d37beca86a21067ecb2d23e3071e82deb
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2023-05-04 18:30:40 -0300

    Add sequelize package and connect to database

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,9 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
+// schema - username - password - options
+const sequelize = new Sequelize('node-complete', 'root', process.env.DB_PASSWORD, {
+  dialect: 'mysql',
   host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: process.env.DB_PASSWORD,
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;

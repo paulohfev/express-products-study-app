6deb35455c807c60d353d51a360d84d64d9b727c
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2023-05-04 17:27:29 -0300

    Update flow to use database and refactor get products logic

--- models/product.js
+++ models/product.js
@@ -1,23 +1,6 @@
-const fs = require('fs');
-const path = require('path');
+const db = require('../utils/database');
 
 const Cart = require('./cart');
-
-const p = path.join(
-  path.dirname(require.main.filename),
-  'data',
-  'products.json'
-);
-
-const getProductsFromFile = (callback) => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      callback([]);
-    } else {
-      callback(JSON.parse(fileContent));
-    }
-  });
-}
 
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
@@ -29,47 +12,18 @@ module.exports = class Product {
   }
 
   save() {
-    getProductsFromFile(products => {
-      if (this.id) {
-        // update existing id. Does not create a new product if there's an id
-        const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-        const updatedProducts = [...products];
-        // this ~> refers to the updated Product instance
-        updatedProducts[existingProductIndex] = this;
-        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-          console.log(err);
-        });
-      } else {
-        this.id = Math.random().toString();
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), (err) => {
-          console.log(err);
-        });
-      }
-    });
+
   }
 
   static deleteById(id) {
-    getProductsFromFile(products => {
-      const product = products.find(prod => prod.id === id);
-      const updatedProducts = products.filter(prod => prod.id !== id);
-      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-        if (!err) {
-          // remove from cart
-          Cart.deleteProduct(id, product.price);
-        }
-      });
-    })
+
   }
 
-  static fetchAll(callback) {
-    getProductsFromFile(callback);
+  static fetchAll() {
+    return db.execute('SELECT * FROM products')
   }
 
-  static findById(id, callback) {
-    getProductsFromFile(products => {
-      const product = products.find(p => p.id === id);
-      callback(product);
-    });
+  static findById(id) {
+
   }
 }

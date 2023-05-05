84c9aefb6884c4749b3ecfa6a55dc92e5c9d4399
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2023-05-05 14:51:30 -0300

    Update Products model to use sequelize

--- models/product.js
+++ models/product.js
@@ -1,34 +1,27 @@
-const db = require('../utils/database');
+const Sequelize = require('sequelize');
 
-const Cart = require('./cart');
+const sequelize = require('../utils/database');
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIcrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  descripton: {
+    type: Sequelize.STRING,
+    allowNull: false,
   }
+});
 
-  save() {
-    // prevent SQL injection ~> use of question marks, each for a field and second argument that 
-    // will contain the values that will replace the question marks
-    return db.execute(
-      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-
-  static deleteById(id) {
-
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products')
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-}
+module.exports = Product;

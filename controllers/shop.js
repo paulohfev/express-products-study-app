ecaa496437a3d5d51ffc15e61252b4166da8fdea
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2023-05-05 17:28:57 -0300

    Add findByPK logic and commented alternative with where SQL using sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,13 +16,23 @@ exports.getProducts = (req, res, next) => {
 }
 
 exports.getProduct = (req, res, next) => {
+  //Alternative
+  // Product.findAll({ where: { id: prodId } })
+  //   .then(products => {
+  //     res.render('shop/product-detail', {
+  //       product: products[0],
+  //       pageTitle: products[0].title,
+  //       path: '/products',
+  //     })
+  //   })
+  //   .catch(err => console.log(err));
+
   // gets the dynamic data through params that will be passed as the id
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
-      console.log(product);
+  Product.findByPk(prodId)
+    .then(product => {
       res.render('shop/product-detail', {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: '/products',
       })

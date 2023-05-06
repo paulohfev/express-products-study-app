b104338a99b36de4e9d3ab6523259813a1338455
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2023-05-05 23:46:33 -0300

    Set up one-to-many relationship between products and user

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const errorController = require('./controllers/error');
 const sequelize = require('./utils/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
-
 app.set('view engine', 'ejs');
 // configs express where to compile templates and where to find them
 app.set('views', 'views');
@@ -23,8 +22,14 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// Relations
+  // CASCADE ~> anything belonging to a user would also be deleted
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 sequelize
-  .sync()
+  // force true overrides tables with new changes in models, etc. (NOT TO BE USED IN PRODUCTION)
+  .sync({ force: true })
   .then(result => {
     // console.log(result);
     app.listen(3000);

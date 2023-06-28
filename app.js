b9cb521094a2c56bea7550ce09e481f922a152e9
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2023-06-28 00:35:33 -0300

    Update users flow with static user on app load and mongodb

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const mongoConnect = require('./utils/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -16,14 +17,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then((user) => {
-  //     // stores the sequelized user in the req user the user field. If done to the body (req.body.user), we would be overriding.
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findById('649b9dad48cb7fd1f7d08ec5')
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);

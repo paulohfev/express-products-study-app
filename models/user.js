b5c6ea29fc06dac72ad1ccfc6976756837c475bf
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2023-08-06 23:32:55 -0300

    Update order to relate with user

--- models/user.js
+++ models/user.js
@@ -79,9 +79,18 @@ class User {
 
   addOrder() {
     const db = getDb();
-    return db
-      .collection('orders')
-      .insertOne(this.cart)
+    return this.getCart()
+      .then(products => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            name: this.name,
+            email: this.email,
+          }
+        };
+        return db.collection('orders').insertOne(order);
+      })
       .then(result => {
         this.cart = {items: []};
         return db
@@ -91,6 +100,11 @@ class User {
             { $set: { cart: {items: [ ]} } }
           );
       });
+  }
+
+  getOrders() {
+    const db = getDb();
+    // return db.collection('orders')
   }
 
   static findById(userId) {

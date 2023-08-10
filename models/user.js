6b0f7d584b836bc9475d9c3917a529088b568575
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2023-08-09 21:25:27 -0300

    Update add product to cart logic to use mongoose

--- models/user.js
+++ models/user.js
@@ -21,6 +21,30 @@ const userSchema = new Schema({
   }
 });
 
+userSchema.methods.addToCart = function(product) {
+  // if index is valid, there is an item in the cart. Otherwise, there is no existing item in the cart yet.
+  const cartProductIndex = this.cart.items.findIndex(cp => {
+    return cp.productId.toString() === product._id.toString();
+  });
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity
+    });
+  }
+  const updatedCart = {
+    items: updatedCartItems
+  };
+  this.cart = updatedCart;
+  return this.save();
+};
+
 module.exports = mongoose.model('User', userSchema);
 
 // const mongodb = require('mongodb');
